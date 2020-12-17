6ac43c7f4f07d185a5ec18853914704ce32ffd97
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-12-17 11:34:41 -0300

    modelo sem restrições

--- ProjetoPO/test/ModeloTeste.py
+++ ProjetoPO/test/ModeloTeste.py
@@ -9,11 +9,18 @@ custoArmazem = np.array([
     [1,4],
     [3,2]
 ])
-
+#Definindo Modelo
+model = LpProblem("Modelo-de-Teste",LpMinimize)
 #Indices Variaveis
 indiceVariaveis = [str(i)+str(j) for j in range(1, nArmazem+1) for i in range(1, nProducao+1)]
 indiceVariaveis.sort()
-
+print("Indices de x: ",indiceVariaveis,"\n")
 #Variaveis de Decisao
-DV_variables = LpVariable.matrix("X", indiceVariaveis, cat = "Integer", lowBound= 0 )
-print(DV_variables)
+xVar = LpVariable.matrix("X", indiceVariaveis, cat = "Integer", lowBound= 0 )
+xVar = np.array(xVar).reshape(2,2)
+print("X: ",xVar,"\n")
+#Funcao Objetivo
+obj_func = lpSum(xVar*custoArmazem)
+print("Funcao objetivo: ",obj_func,"\n")
+model +=  obj_func
+print(model)

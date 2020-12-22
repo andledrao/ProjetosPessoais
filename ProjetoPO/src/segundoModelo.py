60b6827f3d32b83265500b7f7a5956f2509f9cbb
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-12-22 16:53:33 -0300

    restrição 6

--- ProjetoPO/src/segundoModelo.py
+++ ProjetoPO/src/segundoModelo.py
@@ -47,6 +47,12 @@ estoqueMin = np.array([1,1,1])
     #K do artigo
 capacidadeProdutiva = np.array([100,100,100])
 
+    #Capacidade em kg do veículo
+tetaV = np.array([100,120])
+
+    #Peso do produto
+pesoP = np.array([1,2,3])
+
 #Definindo Modelo
 modelo = LpProblem("Modelao-Da-Massa",LpMinimize)
 #Indices Variaveis
@@ -74,42 +80,42 @@ indicesA = [str(r)+str(v) for r in range(1,nRota+1) for v in range(1,nVeiculo+1)
     #Quantidade produzida do item A no periodo t
 xVar = LpVariable.matrix("X", indicesX, cat = "Integer", lowBound= 0 )
 xVar = np.array(xVar).reshape(3,3)
-print("X: ",xVar)
+# print("X: ",xVar)
     #Inventário do item A no final do periodo t
 iVar = LpVariable.matrix("I", indicesI, cat = "Integer", lowBound= 0 )
 iVar = np.array(iVar).reshape(3,3)
-print("I: ",iVar)
+# print("I: ",iVar)
     #Igual a 1 se a linha tiver o setup do item A no periodo t, 0 caso contrário
 yVar = LpVariable.matrix("Y", indicesY, cat = "Binary", lowBound= 0 )
 yVar = np.array(yVar).reshape(3,3)
-print("Y: ",yVar)
+# print("Y: ",yVar)
     #Igual a 1 se houver troca de setup do item A pro item B no periodo t, 0 caso contrário
 zVar = LpVariable.matrix("Z", indicesZ, cat = "Binary", lowBound= 0 )
 zVar = np.array(zVar).reshape(3,3,3)
-print("Z: ",zVar)
+# print("Z: ",zVar)
     #Variável auxiliar para sequênciamento
 piVar = LpVariable.matrix("PI", indicesPI, cat = "Continuous", lowBound= 0 )
 piVar = np.array(piVar).reshape(3,3)
-print("PI: ",piVar)
+# print("PI: ",piVar)
     #Igual a 1 se o arco ij for navegado pela rota R do veículo V, 0 caso contrário
 wVar = LpVariable.matrix("W", indicesW, cat = "Binary", lowBound= 0 )
 wVar = np.array(wVar).reshape(4,4,6)
-print("W: ",wVar)
+# print("W: ",wVar)
     #Quantidade do produto P despachado na rota R pelo veículo V no periodo t
 qVar = LpVariable.matrix("Q", indicesQ, cat = "Integer", lowBound= 0 )
 qVar = np.array(qVar).reshape(3,6,3)
-print("Q: ",qVar)
+# print("Q: ",qVar)
     #Igual a 1 se o nó for visitado pela rota R com o veículo V no periodo t, 0 caso contrário
 sVar = LpVariable.matrix("S", indicesS, cat = "Binary", lowBound= 0 )
 sVar = np.array(sVar).reshape(4,6,3)
-print("S: ",sVar)
+# print("S: ",sVar)
     #Horário de início que o nó I é atendido pela rota R com o veículo V
 uVar = LpVariable.matrix("U", indicesU, cat = "Continuous", lowBound= 0 )
 uVar = np.array(uVar).reshape(4,6)
-print("U: ",uVar)
+# print("U: ",uVar)
 aVar = LpVariable.matrix("A", indicesA, cat = "Binary", lowBound= 0 )
 aVar = np.array(aVar).reshape(6,2)
-print("A: ",aVar)
+# print("A: ",aVar)
 #Funcao Objetivo
 objFunc = lpSum(custoArmazenamento*iVar)+lpSum(tempoSetup*zVar)+lpSum(custoTransporte*wVar)
 # print(objFunc)
@@ -140,5 +146,23 @@ for a in range(nComponente):
         else:
             # print(iVar[a][t-1]+xVar[a][t] == lpSum(proporcaoComponenteProduto[p] * qVar[p][r][t] for p in range(nProduto) for r in range(nRota))+iVar[a][t])
             modelo+=iVar[a][t-1] >= lpSum(proporcaoComponenteProduto[p] * qVar[p][r][t] for p in range(nProduto) for r in range(nRota))
-print(modelo)
+# print(modelo)
+
+#Restrição 4(_C18 até _C72)
+for p in range(nProduto):
+    for r in range(nRota):
+        for t in range(nMes):
+            # print(qVar[p][r][t] <= np.min(tetaV)*sVar[1][r][t])
+            modelo+=qVar[p][r][t] <= np.min(tetaV)*sVar[1][r][t]
+
+#Restrição 5
+# for r in range(nRota):
+#     for s in range(nRota):
+#         uVar[1][s]>=uVar[n+1][r]
+
+#Restrição 6
+
+for r in range(nRota):
+    for p in range(nProduto):
+        print(pesoP[p]*lpSum(qVar[p][r][t] for t in range(nMes)) <= list(tetaV[v]*aVar[r][v]  for v in range(nVeiculo)))
 

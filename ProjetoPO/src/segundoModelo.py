8e9cc5122fb797a85f1232094f0990c88dcb2ffb
André Lebrão <61150819+andledrao@users.noreply.github.com>
2020-12-22 11:41:37 -0300

    funcao objetivo pronta

--- ProjetoPO/src/segundoModelo.py
+++ ProjetoPO/src/segundoModelo.py
@@ -10,38 +10,30 @@ nVeiculo = 2
 nRota = 6
 
     #C do artigo
-custoTransporte = np.array([
-    [np.inf,np.inf,np.inf,np.inf],
-    [np.inf,np.inf,np.inf,np.inf],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [np.inf,np.inf,np.inf,np.inf],
-    [np.inf,np.inf,np.inf,np.inf],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,1,1,1],
-    [np.inf,np.inf,np.inf,np.inf],
-    [np.inf,np.inf,np.inf,np.inf],
-])
-
+custoTransporte = np.zeros((4,4,6))
+for i in range(nNo):
+    for j in range(nNo):
+        for r in range(nRota):
+            if(i==j):
+                custoTransporte[i][j][r] = np.inf
+            else:
+                custoTransporte[i][j][r] = 1
     #Ç do artigo
 
-tempoSetup = np.array([
-    [np.inf,np.inf,np.inf],
-    [1,1,1],
-    [1,1,1],
-    [np.inf,np.inf,np.inf]
-])
+tempoSetup = np.array([[[np.inf,np.inf,np.inf],
+  [1,1,1],
+  [1,1,1]],
+
+ [[1,1,1],
+  [np.inf,np.inf,np.inf],
+  [1,1,1]],
+
+ [[1,1,1],
+  [1,1,1],
+  [np.inf,np.inf,np.inf]]])
 
     #hINDICE do artigo (hA,hB etc)
-custoArmazenamento = np.array([1,2]).reshape(-1,1)
+custoArmazenamento = np.array([1,2,3]).reshape(-1,1)
     
     #rho do artigo
 tempoProcessamento = np.array([1,1,1])
@@ -84,34 +76,45 @@ indicesA = [str(r)+str(v) for r in range(1,nRota+1) for v in range(1,nVeiculo+1)
 #Variaveis de Decisao
     #Quantidade produzida do item A no periodo t
 xVar = LpVariable.matrix("X", indicesX, cat = "Integer", lowBound= 0 )
-xVar = np.array(xVar)
+xVar = np.array(xVar).reshape(3,3)
 print("X: ",xVar)
     #Inventário do item A no final do periodo t
 iVar = LpVariable.matrix("I", indicesI, cat = "Integer", lowBound= 0 )
-iVar = np.array(iVar)
+iVar = np.array(iVar).reshape(3,3)
 print("I: ",iVar)
     #Igual a 1 se a linha tiver o setup do item A no periodo t, 0 caso contrário
 yVar = LpVariable.matrix("Y", indicesY, cat = "Binary", lowBound= 0 )
+yVar = np.array(yVar).reshape(3,3)
 print("Y: ",yVar)
     #Igual a 1 se houver troca de setup do item A pro item B no periodo t, 0 caso contrário
 zVar = LpVariable.matrix("Z", indicesZ, cat = "Binary", lowBound= 0 )
-zVar = np.array(zVar)
+zVar = np.array(zVar).reshape(3,3,3)
 print("Z: ",zVar)
     #Variável auxiliar para sequênciamento
 piVar = LpVariable.matrix("PI", indicesPI, cat = "Continuous", lowBound= 0 )
+piVar = np.array(piVar).reshape(3,3)
 print("PI: ",piVar)
     #Igual a 1 se o arco ij for navegado pela rota R do veículo V, 0 caso contrário
 wVar = LpVariable.matrix("W", indicesW, cat = "Binary", lowBound= 0 )
+wVar = np.array(wVar).reshape(4,4,6)
 print("W: ",wVar)
     #Quantidade do produto P despachado na rota R pelo veículo V no periodo t
 qVar = LpVariable.matrix("Q", indicesQ, cat = "Integer", lowBound= 0 )
-qVar = np.array(qVar)
+qVar = np.array(qVar).reshape(3,6,3)
 print("Q: ",qVar)
     #Igual a 1 se o nó for visitado pela rota R com o veículo V no periodo t, 0 caso contrário
 sVar = LpVariable.matrix("S", indicesS, cat = "Binary", lowBound= 0 )
+sVar = np.array(sVar).reshape(4,6,3)
 print("S: ",sVar)
     #Horário de início que o nó I é atendido pela rota R com o veículo V
-uVar = LpVariable.matrix("U", indicesS, cat = "Continuous", lowBound= 0 )
+uVar = LpVariable.matrix("U", indicesU, cat = "Continuous", lowBound= 0 )
+uVar = np.array(uVar).reshape(4,6)
 print("U: ",uVar)
 aVar = LpVariable.matrix("A", indicesA, cat = "Binary", lowBound= 0 )
+aVar = np.array(aVar).reshape(6,2)
 print("A: ",aVar)
+#Funcao Objetivo
+objFunc = lpSum(custoArmazenamento*iVar)+lpSum(tempoSetup*zVar)+lpSum(custoTransporte*wVar)
+# print(objFunc)
+modelo+=objFunc
+print(modelo)
